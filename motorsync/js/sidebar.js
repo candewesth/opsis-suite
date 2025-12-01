6b4d88606868fdc7716cc7d9c83e09cd66b173df
candewesth <candewesth@gmail.com>
2025-12-01 06:01:45 -0800

    refactor: Add VERDI overrides and update all motorsync pages with sidebar improvements

--- motorsync/js/sidebar.js
+++ motorsync/js/sidebar.js
@@ -25,6 +25,12 @@
       icon: '<svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg>',
     },
     {
+      id: 'notifications',
+      label: 'NotifySync',
+      href: 'NotifySync.html',
+      icon: '<svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path><path d="M13.73 21a2 2 0 0 1-3.46 0"></path></svg>',
+    },
+    {
       id: 'howsync',
       label: 'HowSync',
       href: 'howsync.html',
